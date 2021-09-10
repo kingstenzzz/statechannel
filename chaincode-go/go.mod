ff0ae8058895c15f964e1a4c506d640bb3b06286
kingstenzzz <www.609992452@foxmail.com>
2021-09-10 14:59:38 +0800

    uodate channel

--- chaincode-go/go.mod
+++ chaincode-go/go.mod
@@ -1,8 +1,13 @@
-module github.com/hyperledger/fabric-samples/token-erc-20/chaincode-go
+module github.com/kingstenzzz/statechannel/chaincode-go
 
 go 1.14
 
 require (
 	github.com/hyperledger/fabric-contract-api-go v1.1.0
 	golang.org/x/tools v0.1.0 // indirect
+	github.com/kingstenzzz/statechannel/chaincode-go/chaincode V1.0
+
+)
+replace (
+	github.com/kingstenzzz/statechannel/chaincode-go/chaincode => ./chaincode
 )
--- chaincode-go/token_erc_20.go
+++ chaincode-go/token_erc_20.go
@@ -8,11 +8,15 @@ import (
 	"log"
 
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
-	"github.com/hyperledger/fabric-samples/token-erc-20/chaincode-go/chaincode"
+	//"github.com/hyperledger/fabric-samples/token-erc-20/chaincode-go/chaincode"
+	//"github.com/kingstenzzz/statechannel/chaincode-go/chaincode"
+	"./chaincode"
+
+
 )
 
 func main() {
-	tokenChaincode, err := contractapi.NewChaincode(&chaincode.SmartContract{})
+	tokenChaincode, err := contractapi.NewChaincode(&chaincode.StateChannel{})
 	if err != nil {
 		log.Panicf("Error creating token-erc-20 chaincode: %v", err)
 	}
